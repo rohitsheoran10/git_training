0097664995a80198226629113666c11f5fb55dc9
rohitsheoran10 <rohitsheoran10@gmail.com>
2018-01-26 16:23:20 +0530

    made changes to the code by adding another line

--- sample_app/src/sample_app_package/Sample_App_cls.java
+++ sample_app/src/sample_app_package/Sample_App_cls.java
@@ -3,8 +3,7 @@ package sample_app_package;
 public class Sample_App_cls {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!");
 	}
 
 }
